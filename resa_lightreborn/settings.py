18273ba782a0df247e89a05cd18996249ab8259b
resandadezcaasyam <dezca30@gmail.com>
2023-11-20 16:36:06 +0700

    fixing 3

--- resa_lightreborn/settings.py
+++ resa_lightreborn/settings.py
@@ -134,9 +134,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # STATIC_URL = '/static/'
 
